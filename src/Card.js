2ee5bec851e9a6be1cc08857d023eac8a9465d18
JerinBoyd <maxers@mac.com>
2018-02-08 19:49:55 -0500

    created a custom card compnent and rendered cards from the cards state

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+    render(){
+        return (
+          <p>Card</p>
+
+        );
+
+    }
+}
+
+export default Card;

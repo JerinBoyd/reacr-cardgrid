3c7a21e0f965b03894515e22893dc862264cd900
JerinBoyd <maxers@mac.com>
2018-02-08 20:26:35 -0500

    added styles in card.js for each card

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 class Card extends Component {
     render(){
         return (
-          <p>Card</p>
+          <div className='col-md-3'>
+             <img src={this.props.src} alt=""/>
+             <p>Some things and stuff</p>
+             <img src="https://via.placeholder.com/20x20" alt="" />
+
+          </div>
 
         );
 
